e20db7be81d3d90cd28837d5addc1c2e288ac91d
Noy Tsemach <Noy.Tse@walla.co.il>
2018-04-20 01:58:04 +0300

    ConsoleUI Done! Start with Logic

--- src/Logic/MachineDescriptor/MachineDescriptor.java
+++ src/Logic/MachineDescriptor/MachineDescriptor.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class MachineDescriptor {
     private int RotorsInUseCount;
     private String Alphabet;
-    private List<Rotor> AvaliableRotors;
-    private List<Reflector> AvaliableReflector;
+    private List<Rotor> AvaliableRotors;  //should be map (rotorID, rotor)
+    private List<Reflector> AvaliableReflector; //should be map (reflectorID,reflector)
 
 
     public MachineDescriptor(int rotorsInUseCount, String alphabet, List<Rotor> avaliableRotors, List<Reflector> avaliableReflector) {
