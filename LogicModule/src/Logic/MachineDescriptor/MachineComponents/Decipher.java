9c3abf9079eeb626ab6d0e2793b368afd86837d9
shirlypa <shirlypa@mta.ac.il>
2018-05-19 19:01:04 +0300

    hi

---
+++ LogicModule/src/Logic/MachineDescriptor/MachineComponents/Decipher.java
@@ -0,0 +1,29 @@
+package Logic.MachineDescriptor.MachineComponents;
+
+public class Decipher {
+    private int Agents;
+    private Dictionary Words;
+
+
+    public Decipher(int agents, Dictionary words) {
+        Agents = agents;
+        Words = words;
+    }
+
+
+    public int getAgents() {
+        return Agents;
+    }
+
+    public void setAgents(int agents) {
+        Agents = agents;
+    }
+
+    public Dictionary getWords() {
+        return Words;
+    }
+
+    public void setWords(Dictionary words) {
+        Words = words;
+    }
+}
