4c9d3b506279c840f75170442e17df0ffca24cfe
shirlypa <shirlypa@mta.ac.il>
2018-05-27 15:48:04 +0300

    Merge branch 'master' of https://github.com/shirlypa/Enigma
    # Conflicts:
    #	LogicModule/src/Logic/Dm/DM.java

--- LogicModule/src/Logic/Dm/DM.java
+++ LogicModule/src/Logic/Dm/DM.java
@@ -92,7 +92,7 @@ public class DM extends Thread implements Runnable {
         missionProd.setMissionsToCraateBeforeStartAgents(missionToCreateBeforeStartAgents);
         missionProd.start();
         //Start listening to accomplishedMissions
-        while (!missionProd.getFinish()){
+        while (!missionProd.getFinish()|| mWorkSize/missionSize != accomplishedMissions){
             try {
                 SuccessString successString = validStringQueue.take();
                 synchronized (validStringList) {
